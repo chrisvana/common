ea38cb418f2e8009f704e3bb9d7c3efb6a62f4a4
Chris Van Arsdale <chris.vana@gmail.com>
2013-07-25 16:20:03 -0700

    files

--- file/fileutil.cc
+++ file/fileutil.cc
@@ -10,10 +10,10 @@ namespace file {
 
 std::string ReadFileToStringOrDie(const std::string& filename) {
   std::ifstream ifs(filename.c_str(), std::ifstream::in);
-  CHECK(ifs.is_open());
+  CHECK(ifs.is_open()) << "Could not open file: " << filename;
   std::string ret((std::istreambuf_iterator<char>(ifs) ),
                   (std::istreambuf_iterator<char>()    ) );
-  CHECK(!ifs.bad());
+  CHECK(!ifs.bad()) << "Could not read file: " << filename;
   return ret;
 }
 
