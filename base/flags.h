c22a288bd20e1b91b20b8b6d57f9c8852f9d33cf
Chris Van Arsdale <chris.vana@gmail.com>
2013-08-13 15:40:36 -0700

    flags

---
+++ base/flags.h
@@ -0,0 +1,9 @@
+// Copyright 2013
+// Author: Christopher Van Arsdale
+
+#ifndef _COMMON_BASE_FLAGS_H__
+#define _COMMON_BASE_FLAGS_H__
+
+#include "common/third_party/google/gflags/src/gflags/gflags.h"
+
+#endif  // _COMMON_BASE_FLAGS_H__
