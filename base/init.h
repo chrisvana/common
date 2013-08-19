6f08df73e2c2f941ba186cfe660f9a2850619192
Chris Van Arsdale <chris.vana@gmail.com>
2013-08-19 16:44:40 -0700

    gperftools

--- base/init.h
+++ base/init.h
@@ -4,6 +4,8 @@
 #ifndef _COMMON_BASE_INIT_H__
 #define _COMMON_BASE_INIT_H__
 
+#include "common/third_party/google/init/googleinit.h"  // REGISTER_MODULE_*
+
 void InitProgram();  // no command line parsing.
 void InitProgram(int *argc, char*** argv, bool remove_flags);
 
