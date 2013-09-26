9bfd59b05df40d02c1158f5364ec52397359ad5d
Chris Van Arsdale <chris.vana@gmail.com>
2013-09-26 12:02:37 -0700

    .

---
+++ base/atomicops.h
@@ -0,0 +1,9 @@
+// Copyright 2013
+// Author: Christopher Van Arsdale
+
+#ifndef _COMMON_BASE_ATOMICOPS_H__
+#define _COMMON_BASE_ATOMICOPS_H__
+
+#include "common/third_party/google/gperftools/src/base/atomicops.h"
+
+#endif  // _COMMON_BASE_ATOMICOPS_H__
